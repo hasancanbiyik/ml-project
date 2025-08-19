f56bf24c9e7fcae6c29a62ecb48a996a113cd0dd
hasancanbiyik <biyik@montclair.edu>
2025-08-19 12:06:37 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_namespace_packages, setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This functions returns the list of requirements.
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+return requirements
+
+
+
+
+
+setup(
+    name = 'ml-project',
+    version = 0.0.1,
+    author = "Hasan Can Biyik",
+    author_email = "byk.hasancan@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
+
+
+
+
+
+
+
